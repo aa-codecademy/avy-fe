2c8c1296779abadda4320a51f965b9db7b0abd89
lazaratanasov <lazaratanasov1997@gmail.com>
2026-05-08 12:56:50 +0200

    A2-08 — Export student data

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,7 @@ import adminStudentsController from './controllers/meridian/adminStudentsControl
 import adminStudentDetailController from './controllers/meridian/adminStudentDetailController.js';
 import adminStudentEditController from './controllers/meridian/adminStudentEditController.js';
 import adminStudentImportController from './controllers/meridian/adminStudentImportController.js';
+import adminStudentExportController from './controllers/meridian/adminStudentExportController.js';
 import adminStudentProgrammeController from './controllers/meridian/adminStudentProgrammeController.js';
 import notFoundController from './controllers/notFoundController.js';
 
@@ -73,6 +74,7 @@ function registerRoutes() {
     router.addRoute('/admin/users', adminUsersController, true, ['admin']);
     router.addRoute('/admin/students', adminStudentsController, true, ['admin']);
     router.addRoute('/admin/students/import', adminStudentImportController, true, ['admin']);
+    router.addRoute('/admin/students/export', adminStudentExportController, true, ['admin']);
     router.addRoute('/admin/students/:id/edit', adminStudentEditController, true, ['admin']);
     router.addRoute('/admin/students/:id/programme', adminStudentProgrammeController, true, ['admin']);
     router.addRoute('/admin/students/:id', adminStudentDetailController, true, ['admin']);
